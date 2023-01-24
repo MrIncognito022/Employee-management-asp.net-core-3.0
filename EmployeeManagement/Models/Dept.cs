24ab03503728c065b3bac4464b45f79bcbe4507f
17192 <Qureshi@DESKTOP-TMHGTGB>
2023-01-24 11:07:34 +0500

    Validation for department

--- EmployeeManagement/Models/Dept.cs
+++ EmployeeManagement/Models/Dept.cs
@@ -7,4 +7,4 @@
         IT,
         Payroll
     }
-}
+} 
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -13,6 +13,8 @@ namespace EmployeeManagement.Models
             ErrorMessage = "Invalid Email Format")]
         //[Display(Name="Office Email")]
         public string Email { get; set; }
-        public Dept Department { get; set; }
+        
+        [Required]
+        public Dept? Department { get; set; }
     }
 }
